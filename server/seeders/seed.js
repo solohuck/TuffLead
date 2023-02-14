3cd42d22c1dc3f42605eeb52cc927b43c1536ea3
solo <solohuckstep@gmail.com>
2023-02-13 21:37:32 -0500

    added starter code

---
+++ server/seeders/seed.js
@@ -0,0 +1,4 @@
+const db = require('../config/connection');
+const { User, Subscription } = require('../models');
+const userSeeds = require('./userSeeds.json');
+const subscriptionSeeds = require('./subscriptionSeeds.json');
