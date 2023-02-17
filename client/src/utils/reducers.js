932f064eb3b0a564cb56677cca3638aa24ca3b4c
Blake Burns <bburns@mysgi.com>
2023-02-16 20:17:23 -0500

    added action.js, GlobalState.js, helper.js, and reducers.js

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,63 @@
+import { useReducer } from "react";
+import {
+  UPDATE_PRODUCTS,
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  ADD_MULTIPLE_TO_CART,
+  CLEAR_CART,
+  TOGGLE_CART
+} from "./actions";
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products],
+      };
+
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter(product => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: []
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen
+      };
+
+    default:
+      return state;
+  }
+};
+
+export function useProductReducer(initialState) {
+  return useReducer(reducer, initialState)
+}
